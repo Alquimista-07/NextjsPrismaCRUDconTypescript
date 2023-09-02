7353184759c7e19310773a453fee10d56b052c1d
Alquimista-07 <jdca.07@gmail.com>
2023-09-02 16:50:16 -0500

    14. Obtener tarea por id desde la DB

--- nextjs-prisma-typescript/src/app/api/tasks/[id]/route.ts
+++ nextjs-prisma-typescript/src/app/api/tasks/[id]/route.ts
@@ -8,6 +8,12 @@
 
 import { NextResponse } from "next/server";
 
+// NOTA: Para hacer que estas rutas funciones nosotros vamos a tener que estar realizando consultas a la base de datos
+//       y esto es muy fácil con la ayuda de prisma, y para ello tendríamso que importarlo e instancialo, pero como esto
+//       ya lo teníamos en la configuarción previamente que realizamos en el archivo prisma.ts ubicado en el directorio 
+//       libs entonces simplemente la usamos
+import { prisma } from '@/libs/prisma';
+
 // NOTA: En este caso si recordamos cuando ejecutamos este método GET, me da información de un request
 //       al cual le indicamos su tipo de dato (Request que es una interface global que ya esta disponible
 //       entonces simplemente la utilizamos) pero también puedo acceder al params y este params es el que 
@@ -21,8 +27,18 @@ interface Params {
 
 //       Accederíamos a esta ruta a través de /api/tasks/:id con su petición GET
 //       (Ej: http://localhost:3000/api/tasks/1)
-export function GET( request: Request, { params }: Params ){
-    return NextResponse.json('Obteniendo tarea ' + params.id);
+export async function GET( request: Request, { params }: Params ){
+    // Usamos prisma para las intraciones con la base de datos teniendo en cuenta el tema del async y await
+    // mencionado en los comentarios realizados en el archivo route.ts de la ruta api/tasks y que tiene el
+    // obtener tareas y crear tarea.
+    // Ahora acá en este caso como queremos obtener solo una tarea usamos es el método findFirst y el cual
+    // recibe un objeto por parámetro con una propiedad where en donde podemos decirle que compare algún campo
+    // en este caso es el id que viene por parámetro el cual lo tenemos que transformar a número ya que en el
+    // shema definimos que era Int
+    const task = await prisma.task.findFirst({
+        where: { id: Number(params.id) }
+    })
+    return NextResponse.json( task );
 }
 
 // Función para actualizar una tarea
