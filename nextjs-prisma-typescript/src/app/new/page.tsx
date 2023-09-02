2926885f1dceeb198300dc66286342e195026475
Alquimista-07 <jdca.07@gmail.com>
2023-09-02 12:03:57 -0500

    3. Creación componente NewPage

---
+++ nextjs-prisma-typescript/src/app/new/page.tsx
@@ -0,0 +1,10 @@
+//NOTA: Para visualizar este componente podemo en la url del navegador
+//      indicar el /new (Ej: http://localhost:3000/new)
+
+function NewPage() {
+  return (
+    <div>NewPage</div>
+  )
+}
+
+export default NewPage
