d886b9ead07caaaecbcce4cc2db124a6d6ee87f4
Alquimista-07 <jdca.07@gmail.com>
2023-09-02 17:18:36 -0500

    17. ReactForm - Crear formulario en el front con estilos de TailwindCss

--- nextjs-prisma-typescript/src/app/new/page.tsx
+++ nextjs-prisma-typescript/src/app/new/page.tsx
@@ -3,7 +3,19 @@
 
 function NewPage() {
   return (
-    <div>NewPage</div>
+    <section className="h-screen flex items-center justify-center">
+
+      <form>
+        <input type="text" placeholder="Escribe un titulo"
+               className="px-3 py-1 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-1 focus:ring-sky-300 focus:border-sky-300 text-black block mb-2"/>
+        <textarea placeholder="Escribe la descripción de la tarea"
+                  className="px-3 py-1 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-1 focus:ring-sky-300 focus:border-sky-300 text-black block w-full"/>
+        <button>
+          Crear
+        </button>
+      </form>
+
+    </section>
   )
 }
 
