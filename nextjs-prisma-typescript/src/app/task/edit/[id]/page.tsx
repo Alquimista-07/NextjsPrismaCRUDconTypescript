d7e6f72f7756568d6c45a7121cef5f489d17d367
Alquimista-07 <jdca.07@gmail.com>
2023-09-03 10:16:13 -0500

    28. Creación de componente editar reutilizando el formulario de crear tarea

---
+++ nextjs-prisma-typescript/src/app/task/edit/[id]/page.tsx
@@ -0,0 +1,5 @@
+// Para este componente como nosotros ya habíamos creado un formulario para la creación de tareas, entonces 
+// lo que vamos a hacer es reutilizarlo por lo tanto para ello lo importamos y lo exportamos.
+import newPage from '@/app/new/page';
+
+export default newPage;
